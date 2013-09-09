fc32966e59c92765dad6737021f517edafbf5f3f
juiitamm <juho.tammela@gmail.com>
2013-09-09 21:57:48 +0300

    Added result window to show form data after submitting.

--- vaadin-form-project/src/main/java/vaadinForm/MyVaadinApplication.java
+++ vaadin-form-project/src/main/java/vaadinForm/MyVaadinApplication.java
@@ -1,18 +1,3 @@
-/*
- * Copyright 2009 IT Mill Ltd.
- *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy of
- * the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
- * the License.
- */
 package vaadinForm;
 
 import com.vaadin.Application;
@@ -20,6 +5,8 @@ import com.vaadin.data.util.BeanItem;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Button.ClickEvent;
 import com.vaadin.ui.Form;
+import com.vaadin.ui.GridLayout;
+import com.vaadin.ui.Label;
 import com.vaadin.ui.Window;
 import java.util.Vector;
 
@@ -29,19 +16,30 @@ import java.util.Vector;
 @SuppressWarnings("serial")
 public class MyVaadinApplication extends Application
 {
-    private Window window;
+    private Window formWindow;
+    private Window resultsWindow;
+    private Applicant applicant;
     
     @Override
     public void init()
     {
-        window = new Window("My Vaadin Application");
-        setMainWindow(window);
+        formWindow = new Window("Vaadin form application");
+        setMainWindow(formWindow);
         
+        resultsWindow = new Window("Vaadin form application");
+        
+        
+        Label heading = new Label("Applicant information form");
+        heading.setStyleName("h1");
+        formWindow.addComponent(heading);
+
         Form form = new Form();
         form.setCaption("Applicant information");
-        window.addComponent(form);
+        form.setDescription("Please fill in the required data.");
+        //form.setImmediate(true);
+        formWindow.addComponent(form);
                 
-        Applicant applicant = new Applicant();
+        applicant = new Applicant();
         BeanItem item = new BeanItem(applicant);
         
         Vector order = new Vector();
@@ -54,32 +52,35 @@ public class MyVaadinApplication extends Application
         form.setFormFieldFactory(new ApplicantFormFieldFactory());
         form.setVisibleItemProperties(order);
         
-        /*
-        TextField firstNameField = new TextField("First name");
-        firstNameField.setRequired(true);
-        form.getLayout().addComponent(firstNameField);
-        
-        TextField lastNameField = new TextField("Last name");
-        lastNameField.setRequired(true);
-        form.getLayout().addComponent(lastNameField);
-        
-        OptionGroup genderSelect = new OptionGroup("Gender");
-        genderSelect.addItem("Male");
-        genderSelect.addItem("Female");
-        //genderSelect.setNullSelectionAllowed(false);
-        genderSelect.setNullSelectionItemId("Male");
-        form.getLayout().addComponent(genderSelect);
-        
-        TextArea argumentsTextArea = new TextArea("Why are you applying for this job?");
-        form.getLayout().addComponent(argumentsTextArea);
-
-        */
-        
         Button button = new Button("Send", form, "commit");
         button.addListener(new Button.ClickListener() {
             public void buttonClick(ClickEvent event) {
-//                window.addComponent(new Label("Thank you for clicking"));
+                removeWindow(formWindow);
+                setMainWindow(resultsWindow);
+
+                Label heading = new Label("Applicant information form");
+                heading.setStyleName("h1");
+                resultsWindow.addComponent(heading);
+
                 
+                Label subHeading = new Label("The following data was sent successfully!");
+                subHeading.setStyleName("h2");
+                resultsWindow.addComponent(subHeading);
+                
+                GridLayout grid = new GridLayout(2, 4);
+                resultsWindow.addComponent(grid);
+                
+                grid.addComponent(new Label("First name: "));
+                grid.addComponent(new Label(applicant.getFirstName()));
+                
+                grid.addComponent(new Label("Last name: "));
+                grid.addComponent(new Label(applicant.getLastName()));
+                
+                grid.addComponent(new Label("Gender: "));
+                grid.addComponent(new Label(applicant.getGender()));
+                
+                grid.addComponent(new Label("Arguments: "));
+                grid.addComponent(new Label(applicant.getArguments()));
             }
         });
         form.getLayout().addComponent(button);
